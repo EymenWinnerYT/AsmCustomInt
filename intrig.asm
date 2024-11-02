ebb81f09282afe6c0310efe2de022b5ba6075851
EymenWinnerYT <157110787+EymenWinnerYT@users.noreply.github.com>
2024-11-02 13:53:39 +0200

    Update intrig.asm

--- intrig.asm
+++ intrig.asm
@@ -2,10 +2,11 @@
 ;Created by EymenWinnerYT on 2 November 2024
 ;Compile: nasm -f bin intrig.asm -o intrig.com
 
-bits 16
-org 100h
+bits 16 ;16 bit rel mode
+org 100h ;100th memory adress
 
 int 31h ;Trigger our interupt
 
+;Quit program
 mov ax,4c00h
 int 21h
